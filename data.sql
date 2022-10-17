565477d7243d47d04ec0ac083e79ba1668942960
GordonOtieno <otienogordon95@gmail.com>
2022-10-17 14:31:19 +0300

    Add schema script, queries scripts and data scripts

---
+++ data.sql
@@ -0,0 +1,9 @@
+/* Populate database with sample data. */
+
+INSERT INTO animals (id,name,date_of_birth,Weight_kg,neutered,escape_attempts) VALUES ('1','Agumon','2020-02-03',10.23,'true',0);
+INSERT INTO animals (id,name,date_of_birth,Weight_kg,neutered,escape_attempts) VALUES (2,'Gabumon','2018-11-15',8,'true',2);
+INSERT INTO animals (id,name,date_of_birth,Weight_kg,neutered,escape_attempts) VALUES (3,'Pikachu','2021-07-01',15.04,'false',1);
+INSERT INTO animals (id,name,date_of_birth,Weight_kg,neutered,escape_attempts) VALUES (4,'Devimon','2017-12-05',11,'true',5);
+
+
+

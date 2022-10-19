b3ae414bc4676e3cbead949e88c0c87fc61044dd
GordonOtieno <otienogordon95@gmail.com>
2022-10-19 20:38:13 +0300

    update data file, add queries

--- data.sql
+++ data.sql
@@ -1,18 +1,36 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (id,name,date_of_birth,Weight_kg,neutered,escape_attempts) VALUES (1,'Agumon','2020-02-03',10.23,'true',0);
-INSERT INTO animals (id,name,date_of_birth,Weight_kg,neutered,escape_attempts) VALUES (2,'Gabumon','2018-11-15',8,'true',2);
-INSERT INTO animals (id,name,date_of_birth,Weight_kg,neutered,escape_attempts) VALUES (3,'Pikachu','2021-07-01',15.04,'false',1);
-INSERT INTO animals (id,name,date_of_birth,Weight_kg,neutered,escape_attempts) VALUES (4,'Devimon','2017-12-05',11,'true',5);
+INSERT INTO animals (name,date_of_birth,Weight_kg,neutered,escape_attempts) VALUES ('Agumon','2020-02-03',10.23,'true',0);
+INSERT INTO animals (name,date_of_birth,Weight_kg,neutered,escape_attempts) VALUES ('Gabumon','2018-11-15',8,'true',2);
+INSERT INTO animals (name,date_of_birth,Weight_kg,neutered,escape_attempts) VALUES ('Pikachu','2021-07-01',15.04,'false',1);
+INSERT INTO animals (name,date_of_birth,Weight_kg,neutered,escape_attempts) VALUES ('Devimon','2017-12-05',11,'true',5);
 
-INSERT INTO animals (id,name,date_of_birth,Weight_kg,neutered,escape_attempts) VALUES (5,'Charmander','2020-02-08',-11,'false',0);
-INSERT INTO animals (id,name,date_of_birth,Weight_kg,neutered,escape_attempts) VALUES (6,'Plantmon','2021-11-15',-5.7,'true',2);
-INSERT INTO animals (id,name,date_of_birth,Weight_kg,neutered,escape_attempts) VALUES (7,'Squirtle','1993-04-02',-12.13,'false',3);
-INSERT INTO animals (id,name,date_of_birth,Weight_kg,neutered,escape_attempts) VALUES (8,'Angemon','2005-06-12',-45,'true',1);
-INSERT INTO animals (id,name,date_of_birth,Weight_kg,neutered,escape_attempts) VALUES (9,'Boarmon','2005-06-07',20.4,'true',7);
-INSERT INTO animals (id,name,date_of_birth,Weight_kg,neutered,escape_attempts) VALUES (10,'Blossom','1998-10-13',17,'true',3);
-INSERT INTO animals (id,name,date_of_birth,Weight_kg,neutered,escape_attempts) VALUES (11,'Ditto','2022-05-14',22,'true',4);
+INSERT INTO animals (name,date_of_birth,Weight_kg,neutered,escape_attempts) VALUES ('Charmander','2020-02-08',-11,'false',0);
+INSERT INTO animals (name,date_of_birth,Weight_kg,neutered,escape_attempts) VALUES ('Plantmon','2021-11-15',-5.7,'true',2);
+INSERT INTO animals (name,date_of_birth,Weight_kg,neutered,escape_attempts) VALUES ('Squirtle','1993-04-02',-12.13,'false',3);
+INSERT INTO animals (name,date_of_birth,Weight_kg,neutered,escape_attempts) VALUES ('Angemon','2005-06-12',-45,'true',1);
+INSERT INTO animals (name,date_of_birth,Weight_kg,neutered,escape_attempts) VALUES ('Boarmon','2005-06-07',20.4,'true',7);
+INSERT INTO animals (name,date_of_birth,Weight_kg,neutered,escape_attempts) VALUES ('Blossom','1998-10-13',17,'true',3);
+INSERT INTO animals (name,date_of_birth,Weight_kg,neutered,escape_attempts) VALUES ('Ditto','2022-05-14',22,'true',4);
 
+INSERT INTO owners (full_name,age) VALUES ('Sam Smith',34);
+INSERT INTO owners (full_name,age) VALUES ('Jennifer Orwell',19);
+INSERT INTO owners (full_name,age) VALUES ('Bob',45);
+INSERT INTO owners (full_name,age) VALUES ('Melody Pond',77);
+INSERT INTO owners (full_name,age) VALUES ('Dean Winchester',14);
+INSERT INTO owners (full_name,age) VALUES ('Jodie Whittaker',38);
 
+INSERT INTO species(name) VALUES ('Pokemon');
+INSERT INTO species(name) VALUES ('Digimon');
 
+-- Update species
+UPDATE animals SET species_id = 2 WHERE name LIKE '%mon';
+UPDATE animals SET species_id = 1 WHERE species_id IS NULL;
+
+-- update owners column
+UPDATE animals SET owner_id = 1 WHERE name ='Agumon';
+UPDATE animals SET owner_id = 2 WHERE name ='Gabumon' OR name ='Pikachu';
+UPDATE animals SET owner_id = 3 WHERE name ='Devimon' OR name ='Plantmon';
+UPDATE animals SET owner_id = 4 WHERE name ='Charmander' OR name ='Squirtle' OR name ='Blossom';
+UPDATE animals SET owner_id = 5 WHERE name ='Angemon' OR name ='Boarmon';
 

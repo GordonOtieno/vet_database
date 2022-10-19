d98b460c7575a93ac6bac3bdc22c72e2cd1d7aa3
GordonOtieno <otienogordon95@gmail.com>
2022-10-19 11:58:51 +0300

    modify queries

--- queries.sql
+++ queries.sql
@@ -13,18 +13,21 @@ BEGIN;
 UPDATE animals SET species = 'unspecified';
 SELECT * FROM ANIMALS;
 ROLLBACK;
+SELECT * FROM ANIMALS;
 
 -- tasks2 query 2
 BEGIN;
 UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+SELECT species FROM animals;
 UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
 COMMIT;
-SELECT * FROM ANIMALS;
+SELECT species FROM animals;
 -- take screenshort 1
 
 -- tasks2 query 3
 BEGIN;
 DELETE FROM animals;
+SELECT * FROM ANIMALS;
 ROLLBACK;
 SELECT * FROM ANIMALS;
 -- take screenshort 2
@@ -34,20 +37,23 @@ BEGIN;
 DELETE FROM animals WHERE date_of_birth > '2022-01-01';
 SAVEPOINT SP1;
 UPDATE animals SET weight_kg = (weight_kg*-1);
+SELECT * FROM ANIMALS;
 ROLLBACK TO SP1;
+SELECT * FROM ANIMALS;
 UPDATE animals SET weight_kg = (weight_kg*-1) WHERE weight_kg <0;
 COMMIT;
+SELECT * FROM ANIMALS;
 -- take screenshort 3
 
 -- tasks3 query 1
 SELECT COUNT(*) AS number_of_animals FROM animals;
 SELECT COUNT(*) AS never_tried_escape FROM animals WHERE escape_attempts=0;
 SELECT avg(weight_kg) AS average_weight from animals;
--- Who escapes the most, neutered or not neutered animals?
-SELECT neutered, COUNT(escape_attempts) FROM animals GROUP BY neutered;
--- What is the minimum and maximum weight of each type of animal?
+--Who escapes the most, neutered or not neutered animals?
+SELECT name,neutered, escape_attempts FROM animals WHERE escape_attempts = (SELECT MAX(escape_attempts) FROM animals);
+
+--What is the minimum and maximum weight of each type of animal?
 SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
 
--- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
-SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-01-01' GROUP BY species;
--- take screenshort 4
+--What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth>='1990-1-1' AND date_of_birth <='2000-12-31' GROUP BY species;
